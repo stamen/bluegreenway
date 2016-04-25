d996b3759c9c9eb7a169067c29a304b1a19f7d89
Eric Brelsford <ebrelsford@gmail.com>
2016-04-25 16:36:12 -0400

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -40,6 +40,28 @@ export default class Footer extends React.Component {
 
 		return (
 			<footer className='site-footer'>
+				<div className="site-footer-container">
+					<div className="site-footer-row row">
+						<div className="site-footer-column">
+							<div className="site-footer-advocate">The Blue Greenway is advocated by:</div>
+							<a href="http://sfparksalliance.org" className="site-footer-sfpa-logo"></a>
+						</div>
+						<div className="site-footer-column">
+							<div className="site-footer-header">About</div>
+							<p><a href="http://www.sfparksalliance.org/our-work/blue-greenway">The Blue Greenway</a></p>
+							<p><a href="http://www.sfparksalliance.org/">San Francisco Parks Alliance</a></p>
+						</div>
+						<div className="site-footer-column">
+							<div className="site-footer-header">Contact</div>
+							<p>415.621.3260</p>
+							<p><a href="mailto:feedback@sfparksalliance.org">feedback@sfparksalliance.org</a></p>
+							<p>1663 Mission St, Suite 320<br />San Francisco, CA 94103</p>
+						</div>
+						<div className="site-footer-column">
+							Weather
+						</div>
+					</div>
+				</div>
 			</footer>
 		);
 
