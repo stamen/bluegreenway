b9ea12786722688ff20a18f66cae61526fe1ca6b
Eric Brelsford <ebrelsford@gmail.com>
2016-04-27 17:17:42 -0400

    Load events with new start and end dates

--- src/models/events.js
+++ src/models/events.js
@@ -1,9 +1,17 @@
 import moment from 'moment';
 
+const timestampFormat = 'MM/DD/YYYY - HH:mma';
+
 export function cleanEventItem (item) {
 	item = item.node;
-	// TODO finish massaging into a format that will work in our view
-	return item;
+	let cleaned = {};
+	cleaned.startDate = moment(item['Start Date'], timestampFormat);
+	cleaned.endDate = moment(item['End Date'], timestampFormat);
+	cleaned.category = item.Category;
+	cleaned.cost = item.Cost;
+	cleaned.id = item.id;
+	cleaned.title = item.Title;
+	return cleaned;
 }
 
 export function cleanEventsData (items) {
--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -117,31 +117,7 @@ export default class Events extends React.Component {
 	}
 
 	renderRows (events) {
-
-		events = [
-			{ date: moment('3/16/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 16, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-16' },
-			{ date: moment('3/17/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 17, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-17' },
-			{ date: moment('3/18/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 18, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-18' },
-			{ date: moment('3/19/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 19, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-19' },
-			{ date: moment('3/20/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 20, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-20' },
-			{ date: moment('3/21/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 21, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-21' },
-			{ date: moment('3/22/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 22, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-22' },
-			{ date: moment('3/23/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 23, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-23' },
-			{ date: moment('3/24/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 24, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-24' },
-			{ date: moment('3/25/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 25, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-25' },
-			{ date: moment('3/26/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 26, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-26' },
-			{ date: moment('3/27/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 27, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-27' },
-			{ date: moment('3/28/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 28, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-28' },
-			{ date: moment('3/29/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 29, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-29' },
-			{ date: moment('3/30/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 30, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-30' },
-			{ date: moment('3/31/2016', 'M/D/YYYY'), shortMonth: 'mar', day: 31, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-mar-31' },
-			{ date: moment('4/1/2016', 'M/D/YYYY'), shortMonth: 'apr', day: 1, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-apr-01' },
-			{ date: moment('4/2/2016', 'M/D/YYYY'), shortMonth: 'apr', day: 2, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-apr-02' },
-			{ date: moment('4/3/2016', 'M/D/YYYY'), shortMonth: 'apr', day: 3, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-apr-03' },
-			{ date: moment('4/4/2016', 'M/D/YYYY'), shortMonth: 'apr', day: 4, title: 'California Native Plant Society Volunteer Workparty', time: '12:00 am', cost: 'Free', id: 'cali-apr-04' }
-		];
-
-		events = events.filter((event) => event.date >= this.state.events.startDate && event.date <= this.state.events.endDate);
+		events = events.filter(event => event.startDate >= this.state.events.startDate && event.startDate <= this.state.events.endDate);
 
 		let firstEvent = events[0],
 			secondEvent = events[1],
@@ -198,16 +174,15 @@ export default class Events extends React.Component {
 	}
 
 	renderEvent (event) {
-
 		return (
-			<div className='event-cell three columns' key={ event.id }>
+			<div className='event-cell three columns' key={ event.startDate.format('YYYYMMDD') + event.id }>
 				<div className='event-date'>
-					<span>{ event.shortMonth }</span>
-					<span>{ event.day }</span>
+					<span>{ event.startDate.format('MMM') }</span>
+					<span>{ event.startDate.format('D') }</span>
 				</div>
 				<div className='event-details'>
 					<p className='event-title'>{ event.title }</p>
-					<p>Time: <span>{ event.time }</span></p>
+					<p>Time: <span>{ (event.startDate.hour() === 0 && event.startDate.minute() === 0) ? 'all day' :  event.startDate.format('h:mm a') }</span></p>
 					<p>Cost: <span>{ event.cost }</span></p>
 				</div>
 			</div>
