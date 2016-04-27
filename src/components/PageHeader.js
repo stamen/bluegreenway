ff6934f2f504db790c60c67e8bc214955b6c2f43
Eric Brelsford <ebrelsford@gmail.com>
2016-04-27 11:30:27 -0400

    Darken page headers to increase legibility of text

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -40,9 +40,11 @@ export default class PageHeader extends React.Component {
 		return (
 			<div className='page-header row'>
 				<div className='header-cell'>
-					<div className='nine columns'>
-						<h1>Discover the Blue Greenway</h1>
-						<p>Imagine a network of parks, trails, beaches, and bay access points along 13 miles of San Francisco's southeastern waterfront.</p>
+					<div className="header-shade">
+						<div className='nine columns'>
+							<h1>Discover the Blue Greenway</h1>
+							<p>Imagine a network of parks, trails, beaches, and bay access points along 13 miles of San Francisco's southeastern waterfront.</p>
+						</div>
 					</div>
 				</div>
 			</div>
