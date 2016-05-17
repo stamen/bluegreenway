868b74f22b514c055e63949484aacd929fe85868
Chris Henrick <chrishenrick@gmail.com>
2016-05-17 11:44:12 -0400

    create leaflet maps for each zone panel

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -4,6 +4,8 @@ import MapLayersPicker from '../components/MapLayersPicker';
 import MapOverlay from '../components/MapOverlay';
 import PageHeader from '../components/PageHeader';
 
+import * as tileLayers from '../../static/tileLayers.json';
+
 export default class Projects extends React.Component {
 
 	constructor (props) {
@@ -43,9 +45,7 @@ export default class Projects extends React.Component {
 	}
 
 	componentDidMount () {
-
-		//
-
+		this.createMiniMaps();
 	}
 
 	componentWillUnmount () {
@@ -53,9 +53,33 @@ export default class Projects extends React.Component {
 	}
 
 	componentDidUpdate () {
-
 		//
+	}
 
+	createMiniMaps() {
+		const refs = this.refs;
+		Object.keys(refs).forEach(key => {
+			console.log(key);
+			this.renderMap(key);
+		});
+	}
+
+	renderMap(id) {
+		let basemap = L.tileLayer(tileLayers.layers[1].url);
+
+		let map = L.map(id, {
+			zoom: 12,
+			center: [37.757450, -122.406235],
+			zoomControl: false,
+			scrollwheel: false,
+			dragging: false,
+			touchZoom: false,
+			doubleClickZoom: false,
+			keyboard: false,
+			attributionControl: false
+		});
+
+		map.addLayer(basemap);
 	}
 
 	render () {
@@ -73,18 +97,22 @@ export default class Projects extends React.Component {
 				<div className='row'>
 					<div className='three columns zone-cell'>
 						<h4 className='title'>Mission Bay/ Mission Rock</h4>
+						<div ref='map-mb' id='map-mb'></div>
 						<div className='learn-more'><p>Learn More</p></div>
 					</div>
 					<div className='three columns zone-cell'>
 						<h4 className='title'>Pier 70/Central Waterfront</h4>
+						<div ref='map-p70' id='map-p70'></div>
 						<div className='learn-more'><p>Learn More</p></div>
 					</div>
 					<div className='three columns zone-cell'>
 						<h4 className='title'>India Basin</h4>
+						<div ref='map-ib' id='map-ib'></div>
 						<div className='learn-more'><p>Learn More</p></div>
 					</div>
 					<div className='three columns zone-cell'>
 						<h4 className='title'>Shipyard Candlestick</h4>
+						<div ref='map-sc' id='map-sc'></div>
 						<div className='learn-more'><p>Learn More</p></div>
 					</div>
 				</div>
