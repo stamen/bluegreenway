f2ae16551fb31f7da14f31b634ed6dcbcc006888
Chris Henrick <chrishenrick@gmail.com>
2016-05-31 19:53:19 -0400

    events img placeholders & overlays, fixes #54 & 73

--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -15,6 +15,9 @@ import {
 	getTypesOptions
 } from '../models/events';
 
+// global for tracking which default image to use when no photo is supplied
+let defaultImageIndex = 6;
+
 export default class Events extends React.Component {
 
 	constructor (props) {
@@ -143,6 +146,7 @@ export default class Events extends React.Component {
 			return moment.range(event.startDate, event.endDate).overlaps(currentRange);
 		});
 
+
 		let firstEvent = events[0],
 			secondEvent = events[1],
 			remainingEventRows;
@@ -198,29 +202,44 @@ export default class Events extends React.Component {
 	}
 
 	renderEvent (event) {
+		let eventStyle = { backgroundImage: '' };
+
+		if (event.photoURL) {
+			eventStyle.backgroundImage = `url(${event.photoURL})`;
+		} else {
+			eventStyle.backgroundImage = `url(img/events-default-${defaultImageIndex}.jpg)`;
+			defaultImageIndex -= 1;
+			if (defaultImageIndex === 0) defaultImageIndex = 6;
+		}
+
 		return (
-			<div className='event-cell three columns' key={ event.startDate.format('YYYYMMDD') + event.id }>
-				{ (event.startDate.format('D-MMM') === event.endDate.format('D-MMM')) ?
-					(<div className='event-date'>
-						<span>{ event.startDate.format('MMM') }</span>
-						<span>{ event.startDate.format('D') }</span>
-					</div>) :
-					(<div className='event-date-range'>
-						<div>
-							<span className="event-date-range-month">{ event.startDate.format('MMM') }</span>
-							<span className="event-date-range-day">{ event.startDate.format('D') }</span>
-						</div>
-						<div className="event-date-range-separator">&mdash;</div>
-						<div>
-							<span className="event-date-range-month">{ event.endDate.format('MMM') }</span>
-							<span className="event-date-range-day">{ event.endDate.format('D') }</span>
-						</div>
-					</div>)
-				}
-				<div className='event-details'>
-					<p className='event-title'>{ event.title }</p>
-					<p>Time: <span>{ (event.startDate.hour() === 0 && event.startDate.minute() === 0) ? 'all day' :  event.startDate.format('h:mm a') }</span></p>
-					<p>Cost: <span>{ event.cost }</span></p>
+			<div
+				className='event-cell three columns'
+				key={ event.startDate.format('YYYYMMDD') + event.id }
+				style={eventStyle}>
+				<div className='event-shade'>
+					{ (event.startDate.format('D-MMM') === event.endDate.format('D-MMM')) ?
+						(<div className='event-date'>
+							<span>{ event.startDate.format('MMM') }</span>
+							<span>{ event.startDate.format('D') }</span>
+						</div>) :
+						(<div className='event-date-range'>
+							<div>
+								<span className="event-date-range-month">{ event.startDate.format('MMM') }</span>
+								<span className="event-date-range-day">{ event.startDate.format('D') }</span>
+							</div>
+							<div className="event-date-range-separator">&mdash;</div>
+							<div>
+								<span className="event-date-range-month">{ event.endDate.format('MMM') }</span>
+								<span className="event-date-range-day">{ event.endDate.format('D') }</span>
+							</div>
+						</div>)
+					}
+					<div className='event-details'>
+						<p className='event-title'>{ event.title }</p>
+						<p>Time: <span>{ (event.startDate.hour() === 0 && event.startDate.minute() === 0) ? 'all day' :  event.startDate.format('h:mm a') }</span></p>
+						<p>Cost: <span>{ event.cost }</span></p>
+					</div>
 				</div>
 			</div>
 		);
