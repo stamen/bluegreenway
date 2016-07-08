f2bfc4678ec0d0789c7373eb65bc5527064fe77c
ericsoco <eric@transmote.com>
2016-07-08 16:18:47 -0700

    Social media icons in desktop footer. Closes #177

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import socialMediaLinks from '../../static/socialMediaLinks.json';
 
 /**
  * Footer block at bottom of site, with misc links and current weather/tide conditions
@@ -10,6 +11,13 @@ export default (props) => (
 				<div className="site-footer-column">
 					<div className="site-footer-advocate">The Blue Greenway is advocated by:</div>
 					<a href="http://sfparksalliance.org" className="site-footer-sfpa-logo"></a>
+					<div className='menu-social-media'>
+						<div className='menu-social-media-buttons'>
+							<a href={ socialMediaLinks.facebook } className='menu-social-media-button menu-social-media-facebook'></a>
+							<a href={ socialMediaLinks.twitter } className='menu-social-media-button menu-social-media-twitter'></a>
+							<a href={ socialMediaLinks.instagram } className='menu-social-media-button menu-social-media-instagram'></a>
+						</div>
+					</div>
 				</div>
 				<div className="site-footer-column">
 					<div className="site-footer-header">About</div>
